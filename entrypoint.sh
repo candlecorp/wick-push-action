502145f30422084f2d75b152a13bfa1c320ef640
Fawad Shaikh <fawad@candle.dev>
2023-05-26 16:25:26 -0400

    add pwd

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,11 @@ latest="$2"
 registry_username="$3"
 registry_password="$4"
 
+if [ -z "$manifest_path" ] || [ -z "$latest" ] || [ -z "$registry_username" ] || [ -z "$registry_password" ]; then
+  echo "Error: missing or empty input variables"
+  exit 1
+fi
+
 export OCI_USERNAME="$registry_username"
 export OCI_PASSWORD="$registry_password"
 
@@ -20,11 +25,14 @@ fi
 #change directory to the containing directory of wick file
 manifest_dir=$(dirname "$manifest_path")
 cd "$manifest_dir"
+echo "PWD is $(pwd)"
 
 latest_flag=""
 
 if [ "$latest" = "true" ]; then
     latest_flag="--latest"
 fi
+
+echo "$(basename "$bus_path")"
 
 wick registry push "$(basename "$bus_path")" $latest_flag | grep -Eo 'reference="(.*)"' | cut -d '"' -f2 | head -1
