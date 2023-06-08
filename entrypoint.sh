ee34d6974bf654478d272020d739eb0513ace22f
Fawad Shaikh <fawad@candle.dev>
2023-06-08 14:59:24 -0400

    fix: added multiple tag support

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@
 set -e
 
 manifest_path="$1"
-latest="$2"
-registry_username="$3"
-registry_password="$4"
+registry_username="$2"
+registry_password="$3"
+IFS=' ' read -r -a tags <<< "$4"
 
-if [ -z "$manifest_path" ] || [ -z "$latest" ] || [ -z "$registry_username" ] || [ -z "$registry_password" ]; then
+if [ -z "$manifest_path" ] || [ -z "$registry_username" ] || [ -z "$registry_password" ]; then
   echo "Error: missing or empty input variables"
   exit 1
 fi
@@ -26,14 +26,15 @@ fi
 manifest_dir=$(dirname "$manifest_path")
 cd "$manifest_dir"
 
-latest_flag=""
-
-if [ "$latest" = "true" ]; then
-    latest_flag="--latest"
-fi
+#create additional tags list for image
+tag_flags=""
+for tag in "${tags[@]}"
+do
+    tag_flags="$tag_flags --tag=$tag"
+done
 
 # Store the output in a variable
-output=$(wick registry push "$(basename "$manifest_path")" $latest_flag 2>&1 | grep 'reference' | grep -E '"(\S*)"' | cut -d '"' -f2 | head -1)
+output=$(wick registry push "$(basename "$manifest_path")" $tag_flags 2>&1 | grep 'reference' | grep -E '"(\S*)"' | cut -d '"' -f2 | head -1)
 echo $output
 
 # Write the output to the GITHUB_OUTPUT environment file
